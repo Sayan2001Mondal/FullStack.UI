253ee91c4e66db2eeb988c5ab988a7b9c870643a
SayanGithub1 <sayanmondal2002@gmail.com>
2024-02-27 15:37:13 +0530

    added router config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +13,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'FullStack.UI';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes,RouterModule} from '@angular/router';
+import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        component : EmployeesListComponent
+    }
+];
+
+@NgModule({
+    imports : [RouterModule.forRoot(routes)],
+    exports : [RouterModule]
+})
+export class AppRoutingModule{}
+
