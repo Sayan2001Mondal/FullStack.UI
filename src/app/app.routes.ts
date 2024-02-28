92c06d8ad803b3653b86f95895d48423cacb5971
SayanGithub1 <sayanmondal2002@gmail.com>
2024-02-28 12:00:16 +0530

    added models and updated employees-list with tables

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
     {
         path : '',
         component : EmployeesListComponent
+    },
+    {
+        path : 'employees',
+        component : EmployeesListComponent
     }
 ];
 
--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../../models/employee.model';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-employees-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employees-list.component.html',
   styleUrl: './employees-list.component.css'
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit{
+  employees : Employee[] = [];
+  constructor(){
 
+  }
+  ngOnInit(): void {
+      
+  }
 }
