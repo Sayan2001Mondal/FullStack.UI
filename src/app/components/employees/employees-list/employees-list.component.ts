f581dae5b58d115593f06d140987ef856c003e38
SayanGithub1 <sayanmondal2002@gmail.com>
2024-03-03 12:46:09 +0530

    added edit,delete button

--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../../models/employee.model';
 import { CommonModule } from '@angular/common';
+import { EmployeesService } from '../../../services/employees.service';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-employees-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule,RouterModule],
   templateUrl: './employees-list.component.html',
   styleUrl: './employees-list.component.css'
 })
 export class EmployeesListComponent implements OnInit{
   employees : Employee[] = [];
-  constructor(){
-
+  constructor(private empService: EmployeesService){
+empService.getAllEmployees().subscribe((res:Employee[])=>{
+  this.employees = res;
+})
   }
   ngOnInit(): void {
       
